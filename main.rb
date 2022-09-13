c2374ad56d00e9a36fbb4d538cd28ba88e065c7a
ssathish315 <>
2022-09-13 22:46:32 +0000

    Arrays added

--- main.rb
+++ main.rb
@@ -1,23 +1,44 @@
-#puts "Hello, World!"
-#p "hello"
-#print "hello again"
+# puts "Hello, World!"
+# p "hello"
+# print "hello again"
 
+# name = 'Sathish'
+# puts name
 
-name ="Sathish"
-puts name
+# num = 123
+# cond = true
+# puts name, num, cond
 
-num =123
-cond=true
-puts name,num,cond
+# print "Hello #{name}"
 
-print "Hello #{name}"
+# print 'Enter your name'
+# name = gets.chomp
+# print 'Enter your id'
+# puts "Hello #{name}"
+# id = gets.chomp.to_i
 
-print "Enter your name"
-name = gets.chomp
-print "Enter your id"
-puts "Hello #{name}"
-id = gets.chomp.to_i
+# puts "Hello #{name} your id is #{id} "
 
-puts "Hello #{name} your id is #{id} "
+# Str1 = 'This is a String'.freeze
+# Str2 = 'This is another string'.freeze
 
+# puts Str1, Str2
 
+# Str3 = "This is a
+# multiline string".freeze
+
+# puts Str3
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = Array.new(10)
+puts arr1.size
+puts arr2.length
+
+puts arr1.at(2)
+
+puts arr1.fetch(1)
+
+puts arr1.first
+puts arr1.last
+
+puts arr1.take(2)
