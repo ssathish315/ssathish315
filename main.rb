ba25357ca3cc6ab6281e229a219769750d8b297d
ssathish315 <>
2022-09-13 22:26:56 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,23 @@
+#puts "Hello, World!"
+#p "hello"
+#print "hello again"
+
+
+name ="Sathish"
+puts name
+
+num =123
+cond=true
+puts name,num,cond
+
+print "Hello #{name}"
+
+print "Enter your name"
+name = gets.chomp
+print "Enter your id"
+puts "Hello #{name}"
+id = gets.chomp.to_i
+
+puts "Hello #{name} your id is #{id} "
+
+
